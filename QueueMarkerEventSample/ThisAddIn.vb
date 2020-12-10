3333639162c95bb4b3851cd7acf497205508f700
Nikolay Belykh <nbelyh@gmail.com>
2020-12-10 14:30:04 +0100

    Add project files.

---
+++ QueueMarkerEventSample/ThisAddIn.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.Office.Interop.Visio
+
+Public Class ThisAddIn
+
+    Private Sub Application_MarkerEvent(app As Application, SequenceNum As Integer, ContextString As String) Handles Application.MarkerEvent
+        System.Windows.Forms.MessageBox.Show(ContextString)
+    End Sub
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+End Class
